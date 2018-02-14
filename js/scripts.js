db9165e24a857f699cef0563f2a409cbdf343ba7
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-02-14 14:46:05 +0300

    Add js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+      $("p").click(function() {
+        $("img").show();
+      });
+    });
+    $(document).ready(function() {
+         $("p").click(function() {
+           $(".walrus-showing").toggle();
+           $(".walrus-hidden").toggle();
+         });
+         });
+        
